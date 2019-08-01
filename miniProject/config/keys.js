b693a192c0661a0375a4129d1a5867fa13c1dfad
hrithik86 <hrithikmalik86@gmail.com>
2019-08-01 21:19:37 +0530

    added keys.js file

---
+++ miniProject/config/keys.js
@@ -0,0 +1,6 @@
+keys={
+mongo:"mongodb+srv://ukautomation:ukauto12345@cluster0-w5ddj.mongodb.net/test?retryWrites=true&w=majority",
+mailgun_apikey:'89bcb8345d992e299cc9081132c49a16-52b0ea77-91376c9e',
+mailgun_domain:'sandboxf7816555c39e4be9b8a6a49801bb775b.mailgun.org'
+};
+module.exports=keys;
